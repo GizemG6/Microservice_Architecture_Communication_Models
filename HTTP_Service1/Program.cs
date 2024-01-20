c00c21fc8afb78e9fce8ffcf1db0e22a62ec5132
cang8 <0.gizemgunes@gmail.com>
2024-01-20 18:34:18 +0300

    Add project files.

---
+++ HTTP_Service1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        HttpClient httpClient = new();
+        HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7130/api/values");
+
+        if (response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
+    }
+}
---
+++ Microservice_Architecture_Communication_Models/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice_Architecture_Communication_Models.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(1000); //service1 is on hold
+            return Ok(123);
+        }
+    }
+}
